3a52de2410e4d8167e8304742c07247ec5ca1049
Lorenzo <lorendelfo@protonmail.com>
2021-11-22 22:07:44 +0100

    Pulito il codice

--- othello.java
+++ othello.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 import java.io.IOException;
 public class othello {
     public static int dimensioneCampo = 8;
@@ -8,18 +7,7 @@ public class othello {
     public static int moveCounter = 0;
     public static int mode;
     public static void main(String[] args) {
-        
-        
-        //Random rand = new Random();
-        
-        //int[][] campoVecchio = new int[8][8];
-        
         initCampo(campo);
-        //campo[0][0] = 3;
-        //campo[0][1] = 3;
-        
-        //initCampo(campoConMosse);
-        
         controlloMosse(campoConMosse, campo, moveCounter);
         
         try {
@@ -27,54 +15,35 @@ public class othello {
         }
         catch(IOException e) {
             e.printStackTrace();
-        }
-        
-        int controlloFine = 0;
-        
-        /*while(controlloFine < 2){
-            ClearConsole();
-            
-            int n = controlloMosse(campoConMosse, campo, moveCounter);
-            
-            //System.out.println(n);
-            
-            if(n != 0)
-            {
-                controlloFine = 0;
-                
-                //stampaCampo(campoConMosse);
-                
-                if(mode == 1){
-                    routineUtente(campo, campoConMosse, moveCounter);
-                } else {
-                    if(moveCounter%2 == 0)
-                    {
-                        routineUtente(campo, campoConMosse, moveCounter);
-                    }
-                    else
-                    {
-                        routineBot(campo, campoConMosse, moveCounter);
-                        
-                        try {
-                            Thread.sleep(1 * 1000);
-                        } catch (InterruptedException ie) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
-                }
-            }
-            else
-            {
-                controlloFine++;
-            }
-            
-            moveCounter++;
-        }
-        
-        ControlloVittoria(campo);*/
+        }        
+
     }
     
-    static void ControlloVittoria(int campoConMosse[][]){
+    static int getPscore(){
+        int pscore = 0;
+        for(int i = 0; i < dimensioneCampo; i++){
+            for(int j = 0; j < dimensioneCampo; j++){
+                if(campo[i][j] == 1){
+                    pscore++;
+                }
+            }
+        }
+        return pscore;
+    }
+
+    static int getVscore(){
+        int vscore = 0;
+        for(int i = 0; i < dimensioneCampo; i++){
+            for(int j = 0; j < dimensioneCampo; j++){
+                if(campo[i][j] == 2){
+                    vscore++;
+                }
+            }
+        }
+        return vscore;
+    }
+    
+    static void controlloVittoria(int campoConMosse[][]){
         int uno = 0, due = 0, altro = 0;
 
         for(int i = 0; i < dimensioneCampo; i++)
@@ -127,9 +96,8 @@ public class othello {
         {
             for(int j = 0; j < dimensioneCampo; j++)
             {
-                int cnt = 0;
                 
-                if(!ControlloMossa(campo, i, j, moveCounter%2, 0))
+                if(!controlloMossa(campo, i, j, moveCounter%2, 0))
                 {
                     if(moveCounter%2 == 0)
                     {
@@ -163,18 +131,13 @@ public class othello {
         
         int a = scegliMigliore(mosse);
         
-        /*System.out.println("\n"+mosse[a][2]+" "+ a);
-        
-        char b = Leggi.unChar();*/
-        
-        boolean inutile = ControlloMossa(campo, mosse[a][0], mosse[a][1], moveCounter, 1);
+        boolean inutile = controlloMossa(campo, mosse[a][0], mosse[a][1], moveCounter, 1);
         
         if(moveCounter % 2 == 0){
             campo[mosse[a][0]][mosse[a][1]] = 1;
         } else {
             campo[mosse[a][0]][mosse[a][1]] = 2;
         }
-        //moveCounter++;
     }
     
     static int scegliMigliore(int mosse[][]){
@@ -193,7 +156,6 @@ public class othello {
                 max = mosse[i][2];
                 maxIndice = i;
             }
-            //System.out.println(mosse[i][0]+" "+mosse[i][1]+" "+mosse[i][2]);
         }
         
         for(int i = 0; mosse[i][0] != -1; i++)
@@ -229,7 +191,7 @@ public class othello {
             
             int somma = 0;
             
-            boolean inutile = ControlloMossa(campoProva, mosse[i][0], mosse[i][1], moveCounter, 1);
+            boolean inutile = controlloMossa(campoProva, mosse[i][0], mosse[i][1], moveCounter, 1);
             
             if(campoProva[1][1] == avver && campo[1][1] != avver)
             {
@@ -273,7 +235,7 @@ public class othello {
             }
             else
             {
-                mosse[i][2] = -1/*+ controllaBordoInterno(campoConMosse, mosse, campo, i, moveCounter)*/;
+                mosse[i][2] = -1;
             }
         }
     }
@@ -295,7 +257,7 @@ public class othello {
         
         copiaCampo(campo, campoProva);
         
-        boolean inutile = ControlloMossa(campoProva, mosse[i][0], mosse[i][1], moveCounter, 1);
+        boolean inutile = controlloMossa(campoProva, mosse[i][0], mosse[i][1], moveCounter, 1);
         
         campoProva[mosse[i][0]][mosse[i][1]] = bot;
         
@@ -367,88 +329,16 @@ public class othello {
             avver = 1;
         }
         
-        boolean si = ControlloMossa(campoProva, mosse[i][0], mosse[i][1], moveCounter, 1);
-        
-        /*for(int y = 0; y < dimensioneCampo; y++)
-        {
-            for(int j = 0; j < dimensioneCampo; j++)
-            {
-                System.out.print(campoProva[y][j]+" ");
-            }
-            System.out.println();
-        }*/
-        
-        /*if(campoProva[1][1] == avver && campo[1][1] != avver)
-        {
-            somma++;
-        }
-        
-        if(campoProva[1][6] == avver && campo[1][6] != avver)
-        {
-            somma++;
-        }
-        
-        if(campoProva[6][1] == avver && campo[6][1] != avver)
-        {
-            somma++;
-        }
-        
-        if(campoProva[6][6] == avver && campo[6][6] != avver)
-        {
-            somma++;
-        }
-        
-        if(somma > 0)
-        {
-            return -3;
-        }*/
+        boolean si = controlloMossa(campoProva, mosse[i][0], mosse[i][1], moveCounter, 1);
+
         
         if(mosse[i][1] == 0 || mosse[i][1] == dimensioneCampo-1)
         {
             if(campoConMosse[mosse[i][0]+1][mosse[i][1]] == avver || campoConMosse[mosse[i][0]-1][mosse[i][1]] == avver/* && (mosse[i][0]+mosse[i][1] == 1 || mosse[i][0]+mosse[i][1] == 13 || mosse[i][0]+mosse[i][1] == 6 || mosse[i][0]+mosse[i][1] == 8)*/)
             {
-                /*si = ControlloMossa(campoProva, mosse[i][0], mosse[i][1], moveCounter, 1);
-                
-                for(int y = 0; y < dimensioneCampo; y++)
-                {
-                    for(int j = 0; j < dimensioneCampo; j++)
-                    {
-                        System.out.print(campoProva[y][j]+" ");
-                    }
-                    System.out.println();
-                }*/
                 
                 if(campoProva[mosse[i][0]+1][mosse[i][1]] == avver || campoProva[mosse[i][0]-1][mosse[i][1]] == avver)
                 {
-                    //System.out.print(campoProva[mosse[i][0]+1][mosse[i][1]]+" "+ campoProva[mosse[i][0]-1][mosse[i][1]]+ "   "+ avver+ "    "+ mosse[i][0]+ "   " + mosse[i][1]);
-                    
-                    return -2;
-                }
-                else
-                {
-                     return 1;
-                }
-            }
-        }
-        else
-        {
-            if(campoConMosse[mosse[i][0]][mosse[i][1]+1] == avver || campoConMosse[mosse[i][0]][mosse[i][1]-1] == avver/*  && (mosse[i][0]+mosse[i][1] == 1 || mosse[i][0]+mosse[i][1] == 13 || mosse[i][0]+mosse[i][1] == 6 || mosse[i][0]+mosse[i][1] == 8)*/)
-            {
-                /*si = ControlloMossa(campoProva, mosse[i][0], mosse[i][1], moveCounter, 1);
-                
-                for(int y = 0; y < dimensioneCampo; y++)
-                {
-                    for(int j = 0; j < dimensioneCampo; j++)
-                    {
-                        System.out.print(campoProva[y][j]+" ");
-                    }
-                    System.out.println();
-                }*/
-                
-                if(campoProva[mosse[i][0]][mosse[i][1]+1] == avver || campoProva[mosse[i][0]][mosse[i][1]-1] == avver)
-                {
-                    //System.out.println(campoConMosse[mosse[i][0]][mosse[i][1]+1]+" "+ campoConMosse[mosse[i][0]][mosse[i][1]-1]+ "   "+ avver + "  "+ mosse[i][0]+ "   " + mosse[i][1]);
-                    
                     return -2;
                 }
                 else
@@ -457,52 +347,20 @@ public class othello {
                 }
             }
         }
-        
-        
-        
-        /*if(valore == 1)
+        else
         {
-            if(mosse[i][1] == 1 || mosse[i][1] == 6 || mosse[i][0] == 1 || mosse[i][0] == 6)
+            if(campoConMosse[mosse[i][0]][mosse[i][1]+1] == avver || campoConMosse[mosse[i][0]][mosse[i][1]-1] == avver/*  && (mosse[i][0]+mosse[i][1] == 1 || mosse[i][0]+mosse[i][1] == 13 || mosse[i][0]+mosse[i][1] == 6 || mosse[i][0]+mosse[i][1] == 8)*/)
             {
-                for(int y = 0; y < dimensioneCampo; y++)
+                if(campoProva[mosse[i][0]][mosse[i][1]+1] == avver || campoProva[mosse[i][0]][mosse[i][1]-1] == avver)
                 {
-                    for(int j = 0; j < dimensioneCampo; j++)
-                    {
-                        campoProva[y][j] = campo[y][j];
-                    }
-                }
-                
-                boolean si = ControlloMossa(campoProva, mosse[i][0], mosse[i][1], moveCounter, 1);
-                
-                for(int y = 0; y < dimensioneCampo; y++)
-                {
-                    for(int j = 0; j < dimensioneCampo; j++)
-                    {
-                        System.out.print(campoProva[y][j]+" ");
-                    }
-                    System.out.println();
-                }
-                
-                if(mosse[i][1] == 1 && mosse[i][0] == 0 || mosse[i][1] == 0 && mosse[i][0] == 1)
-                {
-                    
-                }
-                else if(mosse[i][1] == 6 && mosse[i][0] == 0 || mosse[i][1] == 0 && mosse[i][0] == 6)
-                {
-                    
-                }
-                else if(mosse[i][1] == 6 && mosse[i][0] == 7 || mosse[i][1] == 0 && mosse[i][0] == 6)
-                {
-                    
+                    return -2;
                 }
                 else
                 {
-                    
-                    
+                    return 1;
                 }
             }
-        }*/
-        
+        }       
         return 0;
     }
     
@@ -563,7 +421,7 @@ public class othello {
             System.out.println("Colonna");
             y = Leggi.unInt();
             
-            mossa = ControlloMossa(campo, x, y, moveCounter, 1);
+            mossa = controlloMossa(campo, x, y, moveCounter, 1);
             
         }
         while(mossa);
@@ -575,14 +433,12 @@ public class othello {
         }
     }
     
-    static boolean ControlloMossa(int campo[][], int x, int y, int moveCounter, int cambio){
+    static boolean controlloMossa(int campo[][], int x, int y, int moveCounter, int cambio){
         
         int limSu = -1, limGiu = 1, limSin = -1, limDes = 1;
         
         if(x < 0 || x > campo.length || y < 0 || y > campo.length || (campo[x][y] > 0 && campo[x][y] < 3))
         {
-            //ClearConsole();
-            //System.out.println("caso 1");
             return true;
         }
         
@@ -674,14 +530,10 @@ public class othello {
                         case 2:
                         tmp = 5;
                         break;
-                    }
-                    
-                    //System.out.print("tmp: "+tmp + "\n");
-                    
-                    
+                    }                   
+    
                     if(controllaComprese(campo, x, y, tmp, moveCounter%2))
                     {
-                        //System.out.print("si");
                         if(cambio == 1)
                         {
                             cambiaColore(campo, x, y, tmp, moveCounter%2);
@@ -701,10 +553,6 @@ public class othello {
         
         if(cnt == 0)
         {
-            //ClearConsole();
-            
-            //System.out.println("caso 2");
-            
             return true;
         }
         
@@ -839,7 +687,7 @@ public class othello {
             case 1:
                 for(int i = x-1 , j = y-1; i >= 0 && j >= 0 && campo[i][j] != 0 && controllo == 0; i--, j--)
                 {
-                    if(campo[i][j] == giocato && i < x-1/* && j < y-1*/)
+                    if(campo[i][j] == giocato && i < x-1)
                     {
                         controllo = 1;
                     }
@@ -881,7 +729,7 @@ public class othello {
             case 3:
                 for(int i = x-1, j = y+1; i >= 0 && j < dimensioneCampo && campo[i][j] != 0 && controllo == 0; i--, j++)
                 {
-                    if(campo[i][j] == giocato && i < x-1/* && j < y-1*/)
+                    if(campo[i][j] == giocato && i < x-1)
                     {
                         controllo = 1;
                     }
@@ -924,7 +772,7 @@ public class othello {
             case 5:
                 for(int i = x+1, j = y+1; i < dimensioneCampo && j < 8 && campo[i][j] != 0 && controllo == 0; i++, j++)
                 {
-                    if(campo[i][j] == giocato && i > x+1/* && j < y-1*/)
+                    if(campo[i][j] == giocato && i > x+1)
                     {
                         controllo = 1;
                     }
@@ -966,7 +814,7 @@ public class othello {
             case 7:
                 for(int i = x+1, j = y-1; i < dimensioneCampo && j >= 0 && campo[i][j] != 0 && controllo == 0; i++, j--)
                 {
-                    if(campo[i][j] == giocato && i > x+1/* && j < y-1*/)
+                    if(campo[i][j] == giocato && i > x+1)
                     {
                         controllo = 1;
                     }
@@ -1010,12 +858,6 @@ public class othello {
         
         return true;
     }
-    
-    /*
-    static int start(){
-        System.out.println("1- single player\n2- bot");
-        return Leggi.unInt();
-    }*/
     
     static void initCampo(int campo[][]){
         /*
